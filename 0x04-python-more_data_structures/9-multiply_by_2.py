461947c2e0b1ba27a9b54e94dd837915fe93c613
Silverbanks <silver127@gmx.us>
2023-03-20 01:40:02 +0100

    10-best_score.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_d = {}
-    for i in a_dictionary:
-        new_d[i] = a_dictionary[i] * 2
-    return new_d
+    return ({k: a_dictionary[k] * 2 for k in a_dictionary})

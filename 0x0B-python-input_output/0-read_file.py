9ad8beeaef3939299cdb57e700f3a001b3660020
Silverbanks <silver127@gmx.us>
2023-04-13 02:16:50 +0100

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-'''
-Write a function that reads a text file (UTF8)
-and prints it to stdout
-'''
+"""
+0-read_file module
+"""
 
 
 def read_file(filename=""):
-    with open(filename, 'r', encoding='utf-8') as f:
+    """
+    read_file - reads a text file (UTF8) and prints it to stdout
+    Args:
+        filename: name of the file
+    """
+    with open(filename, "r", encoding="UTF-8") as f:
         for line in f:
-            print(line, end='')
-    f.close()
+            print(line, end="")
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""
-7-add_item module
-"""
-import sys
+
+from sys import argv
 import json
-import os.path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-file = "add_item.json"
-json_list = []
+'''
+Write a script that adds all
+arguments to a Python list, and then
+save them to a file
+'''
 
-if os.path.exists(file):
-    json_list = load_from_json_file(file)
 
-for i in range(1, len(sys.argv)):
-    json_list.append(sys.argv[i])
+argc = len(argv)
 
-save_to_json_file(json_list, file)
+filename = 'add_item.json'
+my_list = []
+try:
+    my_list = load_from_json_file(filename)
+except BaseException:
+    pass
+for items in range(1, argc):
+    my_list.append(argv[items])
+save_to_json_file(my_list, filename)
